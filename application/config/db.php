302db748d02bf4654fcf1119ce6d7124fb4714ac
Konstantin Sergeevich Vedenskiy <spaghetti.coder.ru@gmail.com>
2015-01-11 22:55:30 +0200

    Add database settings

---
+++ application/config/db.php
@@ -0,0 +1,6 @@
+<?php // Database configs
+
+define('DB_DSN',     'mysql:dbname=db_jobs_service;host=localhost');
+define('DB_USER',    'u_jobs_service');
+define('DB_PASS',    'jobs_service');
+define('DB_CHATSET', 'UTF8');
