733ebb075f37912b0e462bad63d57620c4ccac5c
Konstantin Sergeevich Vedenskiy <spaghetti.coder.ru@gmail.com>
2015-01-11 22:11:55 +0200

    Create Jobs controller stub

---
+++ application/classes/controllers/JobsController.php
@@ -0,0 +1,11 @@
+<?php
+
+class JobsController extends BaseServiceController {
+    public function action_index() {
+        
+    }
+    
+    public function action_view($id) {
+        
+    }
+}
