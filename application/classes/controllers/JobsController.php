c7696bb363396669218b5834976347754d943c1e
Konstantin Sergeevich Vedenskiy <spaghetti.coder.ru@gmail.com>
2015-01-12 14:28:18 +0200

    Jobs@index prints jobs list

--- application/classes/controllers/JobsController.php
+++ application/classes/controllers/JobsController.php
@@ -1,8 +1,20 @@
 <?php
 
 class JobsController extends BaseServiceController {
-    public function action_index() {
+    /**
+     * @var Job
+     */
+    private $jobsModel;
+    
+    public function __construct() {
+        parent::__construct();
         
+        $this->jobsModel = new Job;
+    }
+    
+    public function action_index() {
+        $jobs = $this->jobsModel->retrieveAll();
+        exit(json_encode($jobs));
     }
     
     public function action_view($id) {
