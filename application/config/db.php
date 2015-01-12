87a0a37ae4379d01a27f711497f26cc073fb57b7
Konstantin Sergeevich Vedenskiy <spaghetti.coder.ru@gmail.com>
2015-01-12 16:38:46 +0200

    Add link to PDO page in db config file

--- application/config/db.php
+++ application/config/db.php
@@ -1,5 +1,8 @@
 <?php // Database configs
 
+// This is db access credentials configuration.
+// See http://php.net/manual/ru/pdo.construct.php for more info
+
 define('DB_DSN',     'mysql:dbname=db_jobs_service;host=localhost');
 define('DB_USER',    'u_jobs_service');
 define('DB_PASS',    'jobs_service');
