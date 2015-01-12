f3249ed2408eda389afb0acb12afcecf068f53e1
Konstantin Sergeevich Vedenskiy <spaghetti.coder.ru@gmail.com>
2015-01-12 13:58:34 +0200

    Add controllers directory to autoload

--- application/autoloads.php
+++ application/autoloads.php
@@ -2,6 +2,7 @@
 
 // List autoload dirs bellow 
 return array(
+    APPDIR . '/classes/controllers',
     APPDIR . '/classes/models',
     APPDIR . '/classes/utils',
     APPDIR . '/classes/exceptions',
